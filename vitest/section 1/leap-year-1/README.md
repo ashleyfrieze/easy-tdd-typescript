b845d3a935cb07506e39b4da99ff8819d45177ca
Ashley Frieze <ashley@incredible.org.uk>
2024-06-06 12:34:29 +0100

    Leap year 1 example

---
+++ vitest/section 1/leap-year-1/README.md
@@ -0,0 +1,1 @@
+# Leap Year Primary Example
---
+++ vitest/section 1/leap-year-1/leap-year.test.ts
@@ -0,0 +1,15 @@
+import { isLeapYear } from './leap-year';
+
+describe('Leap algorithm', () => {
+  it('detects a leap year', () => {
+    expect(isLeapYear(1996)).toBeTruthy();
+  });
+
+  it('detects a non leap year', () => {
+    expect(isLeapYear(1997)).toBeFalsy();
+  });
+
+  it('does not think a century year is a leap year', () => {
+    expect(isLeapYear(1900)).toBeFalsy();
+  })
+});
