8bca8ef6d45cb8cd8d4710c3f96803c7cff95786
Ashley Frieze <ashley@incredible.org.uk>
2024-06-13 10:49:16 +0100

    Solve the employees problem

---
+++ vitest/section 3/org-chart-2/src/EmployeeStore.ts
@@ -0,0 +1,59 @@
+import { Employee } from './types';
+
+export class EmployeeStore {
+  private employees = new Map<string, Employee>();
+  private managersOf = new Map<string, string>();
+  private managees = new Map<string, string[]>();
+
+  add(employee: Employee) {
+    this.employees.set(employee.id, employee);
+  }
+
+  get(id: string): Employee | undefined {
+    return this.employees.get(id);
+  }
+
+  getManager(id: string): Employee | undefined {
+    const managerId = this.managersOf.get(id);
+    if (managerId) {
+      return this.get(managerId);
+    }
+    return undefined;
+  }
+
+  addRelationship({ manager, managed }: { manager: string; managed: string }) {
+    if (!this.get(manager)) {
+      throw new Error(`${manager} does not exist`);
+    }
+    if (!this.get(managed)) {
+      throw new Error(`${managed} does not exist`);
+    }
+    this.managersOf.set(managed, manager);
+    if (!this.managees.has(manager)) {
+      this.managees.set(manager, []);
+    }
+    this.managees.get(manager)?.push(managed);
+  }
+
+  getManageesOf(id: string): Employee[] {
+    return (this.managees.get(id) || [])
+      .map((employeeId) => this.get(employeeId))
+      .filter((item): item is Employee => Boolean(item));
+  }
+
+  getTeamMembers(employeeId: string): Employee[] {
+    const manager = this.getManager(employeeId);
+    if (!manager) {
+      return [];
+    }
+    return this.getManageesOf(manager.id);
+  }
+
+  getOrgChart(manager: string) {
+    // flatmap requires higher ES level
+    return this.getManageesOf(manager).flatMap((employee) => [
+      employee,
+      ...this.getManageesOf(employee.id),
+    ]);
+  }
+}
