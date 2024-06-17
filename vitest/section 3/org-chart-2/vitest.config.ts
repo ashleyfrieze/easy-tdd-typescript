1e6f7e14782e73f7a732d8abbccb281dd1d1a967
Ashley Frieze <ashley@incredible.org.uk>
2024-06-17 14:44:20 +0100

    Lecture for test control moved to section 3

--- vitest/section 3/org-chart-2/vitest.config.ts
+++ vitest/section 3/org-chart-2/vitest.config.ts
@@ -4,5 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
     globals: true,
+    reporters: ['default', ['junit', { suiteName: 'OrgChart' }]],
+    outputFile: 'junit.xml',
   },
 });
--- vitest/section 4/org-chart-2/src/employee-test-factory.ts
+++
@@ -1,4 +0,0 @@
-import emp1 from './data/emp1.json';
-import { cloneDeep } from 'lodash';
-
-export const createEmployee = (id: string) => ({ ...cloneDeep(emp1), id });
